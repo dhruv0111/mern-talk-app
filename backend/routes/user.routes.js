a8eb0c7afdf9851dbadfb21f5bf12a3aaae4d284
dhruv0111 <DHRUVSHARMAAUGUST2003@GMAIL.COM>
2024-04-13 10:49:32 +0530

    Deployment

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,8 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { getUserForSidebar } from "../controllers/user.controller.js";
+import { getUsersForSidebar } from "../controllers/user.controller.js";
 const router = express.Router();
 
-router.get("/",protectRoute,getUserForSidebar);
+router.get("/",protectRoute,getUsersForSidebar);
 
 export default router;
